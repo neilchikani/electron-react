ffef58e0d1368efa1f0729d0b8c630da89f02ffa
nilay_ii5 <chikaninilay@gmail.com>
2017-01-24 09:12:36 +0530

    "changes"

--- webpack.config.js
+++ webpack.config.js
@@ -19,14 +19,7 @@ module.exports = {
 
   module: {
     loaders: [
-      {
-        test: /\.jsx?$/,
-        loader: 'babel-loader',
-        exclude: /node_modules/,
-        query: {
-          presets: ['react', 'es2015']
-        }
-      },
+      {test: /\.jsx?$/, loader: 'babel-loader', exclude: /node_modules/, query: {presets: ['react', 'es2015']}},
       { test: /\.css$/, loader: 'style-loader!css-loader' },
       { test: /\.less$/, loader: 'style-loader!css-loader!less-loader'}
     ]
